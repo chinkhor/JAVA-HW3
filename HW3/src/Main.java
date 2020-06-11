078d4d8d6c006bef8cb941c6b6f03919ba584eb2
chinkhor <chin.kooi.khor@gmail.com>
2020-06-11 09:32:24 +0800

    added textfield

--- HW3/src/Main.java
+++ HW3/src/Main.java
@@ -1,21 +1,29 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		//create panel
 		JPanel panel = new JPanel();
 		panel.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
 		panel.setLayout(new FlowLayout(FlowLayout.CENTER));
 		
-		JFrame frame = new JFrame();
+		// create TextField
+		JTextField txtf = new JTextField();
+		txtf.setPreferredSize(new Dimension(400,50));
+		panel.add(txtf);
 		
+		// create frame
+		JFrame frame = new JFrame();
 		frame.setTitle("Calculator");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(480,480);
