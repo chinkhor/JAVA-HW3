0fccf8aa89cfa41a178c8f81d4ae1f14324f820b
chinkhor <chin.kooi.khor@gmail.com>
2020-06-11 09:12:07 +0800

    Initial Commit for HW3

---
+++ HW3/src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		
+		frame.setTitle("Calculator");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(480,480);
+		frame.setVisible(true);
+
+	}
+
+}
