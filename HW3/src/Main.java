72455ef1aaccd5e7535e3b45f9720db41a370517
chinkhor <chin.kooi.khor@gmail.com>
2020-06-11 09:20:32 +0800

    Added panel

--- HW3/src/Main.java
+++ HW3/src/Main.java
@@ -1,15 +1,27 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createLineBorder(Color.ORANGE));
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER));
 		
 		JFrame frame = new JFrame();
 		
 		frame.setTitle("Calculator");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(480,480);
+		frame.add(panel, BorderLayout.CENTER);
 		frame.setVisible(true);
+		
 
 	}
 
